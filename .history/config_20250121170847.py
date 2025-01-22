787be536404d14cb295dc13342ea27f585b78286
Patzanovsky Carla (CSC FI BE EA) <Carla.Patzanovsky@infineon.com>
2025-01-22 19:56:11 +0100

    Delete all previous files from the repository

--- .history/config_20250121170847.py
+++
@@ -1,7 +0,0 @@
-DATABASE_CONFIG = {
-    'dbname': 'prod_Garbage',
-    'user': 'postgres',
-    'password': 'postgres',
-    'host': 'localhost',
-    'port': '5432'
-}
--- s2i/README.md
+++
@@ -1,2 +0,0 @@
-# S2I Configuration for Flask Application
-
